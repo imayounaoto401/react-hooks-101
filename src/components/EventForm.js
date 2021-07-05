9e5f8312a3f83ab297e2c593bc242b699e775f7b
naoto <imayounaoto401@gmail.com>
2021-07-05 15:07:08 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,7 @@
 import React, {useState,useContext} from "react";
-import {CREATE_EVENT, DELETE_ALL_EVENTS} from "../actions";
+import {CREATE_EVENT, DELETE_ALL_EVENTS,ADD_OPERATION_LOG,DELETE_ALL_OPERATION_LOGS} from "../actions";
 import AppContext from "./contexts/AppContext";
+import {timeCurrentIso8601} from "../utils";
 
 const EventForm = () => {
   const [title, setTitle] = useState('')
@@ -15,6 +16,13 @@ const EventForm = () => {
       title,
       body
     })
+
+    dispatch({
+      type:ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operationAt: timeCurrentIso8601()
+    })
+
     setTitle('')
     setBody('')
   }
@@ -25,6 +33,11 @@ const EventForm = () => {
     if(result) {
       dispatch({
         type: DELETE_ALL_EVENTS
+      })
+      dispatch({
+        type:ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operationAt: timeCurrentIso8601()
       })
     }
   }
